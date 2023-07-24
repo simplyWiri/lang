b8969dc1f8c18133f1cc49e1d052bb8811c8a6a1
simplyWiri <finley.gooding@hotmail.com>
2023-07-24 21:00:56 +1000

    Add utility header, add implementation for a tagged (or discriminated) pointer type

---
+++ src/util.h
@@ -0,0 +1,61 @@
+#include <cstdint>
+#include <type_traits>
+
+namespace util {
+
+// a very simple non-owning pointer which can take one of two values. Any attempt to access a type not in the template
+// will result in a compilation error. Guaranteed to fit within a single 8-byte pointer.
+template<typename First, typename Second>
+class t_ptr {
+private:
+    constexpr static uint64_t TAG_MASK = 1;
+    constexpr static uint64_t POINTER_MASK = ~TAG_MASK;
+
+    intptr_t bits_;
+
+    void* pointer() const {
+        return reinterpret_cast<void*>(bits_ & POINTER_MASK);
+    }
+
+    int tag() const {
+        return bits_ & TAG_MASK;
+    }
+
+    template<typename T>
+    static constexpr bool validType() {
+        return std::is_same_v<T, First> || std::is_same_v<T, Second>;
+    }
+
+    template<typename T>
+    void set(T* pointer) {
+        static_assert(validType<T>());
+        bits_ = reinterpret_cast<intptr_t>(pointer);
+        bits_ |= std::is_same_v<T, First>;
+    }
+
+public:
+    template<typename T>
+    t_ptr(T* pointer) {
+        set(pointer);
+    }
+
+    template<typename T>
+    bool holds() const {
+        static_assert(validType<T>());
+        return tag() == std::is_same_v<T, First>;
+    }
+
+    template<typename T>
+    T* get() const {
+        static_assert(validType<T>());
+        return holds<T>() ? reinterpret_cast<T*>(pointer()) : nullptr;
+    }
+
+    template<typename T>
+    t_ptr& operator=(T* pointer) {
+        static_assert(validType<T>());
+        set(pointer);
+    }
+};
+
+}
---
+++ test/util_tests.cpp
@@ -0,0 +1,30 @@
+#include <doctest.h>
+#include "util.h"
+
+TEST_SUITE_BEGIN("Utility Tests");
+
+TEST_CASE("Tagged Pointer") {
+    int integerValue = 5;
+    util::t_ptr<int, float> intPointer{&integerValue};
+
+    CHECK(intPointer.holds<int>());
+    CHECK_EQ(*intPointer.get<int>(), integerValue);
+    CHECK_EQ(intPointer.get<float>(), nullptr);
+
+    float floatValue = 6.0;
+    util::t_ptr<int, float> floatPointer{&floatValue};
+
+    CHECK(floatPointer.holds<float>());
+    CHECK_EQ(*floatPointer.get<float>(), floatValue);
+    CHECK_EQ(floatPointer.get<int>(), nullptr);
+
+    SUBCASE("operator=") {
+        util::t_ptr<int, float> a = intPointer;
+        CHECK(a.holds<int>());
+
+        util::t_ptr<int, float> b = floatPointer;
+        CHECK(b.holds<float>());
+    }
+}
+
+TEST_SUITE_END();
